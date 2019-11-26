3a444a8d2862c7b9d3b882e8b914be9a358b722a
Alessandro Eliot <alessandroeliot@gmail.com>
2019-11-26 14:25:35 +1100

    Don't mind me, just using this as web hosting lol

---
+++ build/precache-manifest.18b77b89d2caa9390f2d33e6af196694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d24c5d2afad1fe9a3870aa26c04fcc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7016ed30fbb553af137",
+    "url": "./static/css/main.78dc428e.chunk.css"
+  },
+  {
+    "revision": "a878b034887728463ea8",
+    "url": "./static/js/2.36e87ba2.chunk.js"
+  },
+  {
+    "revision": "e7016ed30fbb553af137",
+    "url": "./static/js/main.f041a106.chunk.js"
+  },
+  {
+    "revision": "15dd46cb7706347d8809",
+    "url": "./static/js/runtime-main.7649e152.js"
+  }
+]);

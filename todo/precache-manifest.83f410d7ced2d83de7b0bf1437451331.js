a924ee4ab681ea53f6417eb7f9632c7de2f96868
Alessandro Eliot <alessandroeliot@gmail.com>
2019-12-08 20:59:39 +1100

    Added a ToDo list which works pretty okay.
    To use it go to diordnas.github.io/todo

---
+++ todo/precache-manifest.83f410d7ced2d83de7b0bf1437451331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2c7c3c0d917bbb83ea0af19827a922",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f04a8f48615edb1d10f",
+    "url": "./static/css/main.21d68914.chunk.css"
+  },
+  {
+    "revision": "ed31b24867bbae5e4d6f",
+    "url": "./static/js/2.b148f1da.chunk.js"
+  },
+  {
+    "revision": "5f04a8f48615edb1d10f",
+    "url": "./static/js/main.8fa407be.chunk.js"
+  },
+  {
+    "revision": "466f34e636655b9b9804",
+    "url": "./static/js/runtime-main.25f2b03b.js"
+  }
+]);
